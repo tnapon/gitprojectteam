789ed404f4306a7f4afd7ee6ac06eb20d0f829e7
Chaleomrat <614259007@webmail.npru.ac.th>
2020-09-03 12:13:45 +0700

    Update Use Calculator

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ public class Calculator{
 	private int po1,op2;
 
 	public int multiply(int num1,int num2){
-		return 0;
+		return num1 * num2;
 	}
 	public int add(int num1,int num2){
 		return num1 + num2;
